0eb771c9aad4fdfe8e729ccfd8c4ec345a7e6581
lenovo <oneshot436@gmail.com>
2022-11-22 10:37:15 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import MainRoutes from './routes/MainRoutes';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <MainRoutes />
+    </>
+  );
 };
 
 export default App;
---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AuthPage from '../pages/AuthPage';
+import HomePage from '../pages/HomePage';
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    { link: '/', element: <HomePage />, id: 1 },
+    { link: '/auth', element: <AuthPage />, id: 2 },
+  ];
+  return (
+    <>
+      <Routes>
+        {PUBLIC_ROUTES.map((item) => (
+          <Route path={item.link} element={item.element} key={item.id} />
+        ))}
+      </Routes>
+    </>
+  );
+};
+
+export default MainRoutes;
