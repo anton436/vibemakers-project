350c6b5174f4de1c870de3a5b786a798e4736349
dxsya <dxgdxd@gmail.com>
2022-11-24 00:19:16 +0600

    .

--- src/contexts/ProductContextProvider.js
+++ src/contexts/ProductContextProvider.js
@@ -17,33 +17,16 @@ const reducer = (state = INIT_STATE, action) => {
     switch (action.type) {
         case ACTIONS.GET_PRODUCTS:
             return { ...state, products: action.payload };
+
         case ACTIONS.GET_PRODUCT_DETAILS:
             return { ...state, productDetails: action.payload };
+
         default:
             return state;
     }
 };
 
-const INIT_STATE = {
-  products: [],
-  productDetails: {},
-};
-
-const reducer = (state = INIT_STATE, action) => {
-  switch (action.type) {
-    case ACTIONS.GET_PRODUCTS:
-      return { ...state, products: action.payload };
-
-    case ACTIONS.GET_PRODUCT_DETAILS:
-      return { ...state, productDetails: action.payload };
-
-    default:
-      return state;
-  }
-};
-
 const ProductContextProvider = ({ children }) => {
-<<<<<<< HEAD
     const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
     async function addProduct(newProduct) {
@@ -78,42 +61,6 @@ const ProductContextProvider = ({ children }) => {
             {children}
         </productContext.Provider>
     );
-=======
-  const [state, dispatch] = useReducer(reducer, INIT_STATE);
-
-  async function addProduct(newProduct) {
-    await axios.post(JSON_API_PRODUCTS, newProduct);
-  }
-
-  async function getProducts() {
-    const { data } = await axios(JSON_API_PRODUCTS);
-
-    dispatch({
-      type: ACTIONS.GET_PRODUCTS,
-      payload: data,
-    });
-  }
-
-  async function getProductDetails(id) {
-    const { data } = await axios(`${JSON_API_PRODUCTS}/${id}`);
-
-    dispatch({
-      type: ACTIONS.GET_PRODUCT_DETAILS,
-      payload: data,
-    });
-  }
-
-  const values = {
-    addProduct,
-    products: state.products,
-    productDetails: state.productDetails,
-    getProducts,
-    getProductDetails,
-  };
-  return (
-    <productContext.Provider value={values}>{children}</productContext.Provider>
-  );
->>>>>>> a88a44147a481a229deb930536db5e64443d2acb
 };
 
 export default ProductContextProvider;
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -2,19 +2,11 @@ import React from 'react';
 import ProductDetails from '../components/Products/ProductDetails';
 
 const ProductDetailsPage = () => {
-<<<<<<< HEAD
     return (
         <div>
             <ProductDetails />
         </div>
     );
-=======
-  return (
-    <div>
-      <ProductDetails />
-    </div>
-  );
->>>>>>> a88a44147a481a229deb930536db5e64443d2acb
 };
 
 export default ProductDetailsPage;
