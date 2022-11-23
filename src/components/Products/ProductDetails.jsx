b8a8c0e60a787a4c4519d01be6f91912564c1726
lenovo <oneshot436@gmail.com>
2022-11-23 13:45:48 +0600

    .

--- src/components/Products/ProductDetails.jsx
+++ src/components/Products/ProductDetails.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useProducts } from '../../contexts/ProductContextProvider';
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { getProductDetails, productDetails } = useProducts();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    getProductDetails(id);
+  }, []);
+  return (
+    <div>
+      {productDetails.name} {productDetails.price}
+    </div>
+  );
 };
 
 export default ProductDetails;
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ProductDetails from '../components/Products/ProductDetails';
 
 const ProductDetailsPage = () => {
-  return <div>ProductDetailsPage</div>;
+  return (
+    <div>
+      <ProductDetails />
+    </div>
+  );
 };
 
 export default ProductDetailsPage;
