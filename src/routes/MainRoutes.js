bc56251f637ac01ee4e21fd701136d695fa9641f
Your Name <you@example.com>
2022-11-24 23:21:18 +0600

    contactus save

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AboutUsPage from '../pages/AboutUsPage';
-import AdminPage from '../pages/AdminPage';
-import AuthPage from '../pages/AuthPage';
-import ContactUsPage from '../pages/ContactUsPage';
-import EditProductPage from '../pages/EditProductPage';
-import HomePage from '../pages/HomePage';
-import NotFoundPage from '../pages/NotFoundPage';
-import ProductDetailsPage from '../pages/ProductDetailsPage';
-import ProductsPage from '../pages/ProductsPage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutUsPage from "../pages/AboutUsPage";
+import AdminPage from "../pages/AdminPage";
+import AuthPage from "../pages/AuthPage";
+import ContactUsPage from "../pages/ContactUsPage";
+import EditProductPage from "../pages/EditProductPage";
+import HomePage from "../pages/HomePage";
+import NotFoundPage from "../pages/NotFoundPage";
+import ProductDetailsPage from "../pages/ProductDetailsPage";
+import ProductsPage from "../pages/ProductsPage";
 
 const MainRoutes = () => {
-    const PUBLIC_ROUTES = [
-        { link: '/', element: <HomePage />, id: 1 },
-        { link: '/auth', element: <AuthPage />, id: 2 },
-        { link: '/admin', element: <AdminPage />, id: 3 },
-        { link: '/products', element: <ProductsPage />, id: 4 },
-        { link: '/products/:id', element: <ProductDetailsPage />, id: 5 },
-        { link: '*', element: <NotFoundPage />, id: 6 },
-        { link: '/edit/:id', element: <EditProductPage />, id: 7 },
-<<<<<<< HEAD
-        { link: '/contactus', element: <ContactUsPage/>, id: 8 },
-=======
-        { link: '/aboutus', element: <AboutUsPage />, id: 7 },
->>>>>>> d3c7c21315029ac418f55cd74447ab6c6ee60c80
-    ];
-    return (
-        <>
-            <Routes>
-                {PUBLIC_ROUTES.map((item) => (
-                    <Route
-                        path={item.link}
-                        element={item.element}
-                        key={item.id}
-                    />
-                ))}
-            </Routes>
-        </>
-    );
+  const PUBLIC_ROUTES = [
+    { link: "/", element: <HomePage />, id: 1 },
+    { link: "/auth", element: <AuthPage />, id: 2 },
+    { link: "/admin", element: <AdminPage />, id: 3 },
+    { link: "/products", element: <ProductsPage />, id: 4 },
+    { link: "/products/:id", element: <ProductDetailsPage />, id: 5 },
+    { link: "*", element: <NotFoundPage />, id: 6 },
+    { link: "/edit/:id", element: <EditProductPage />, id: 7 },
+
+    { link: "/contactus", element: <ContactUsPage />, id: 8 },
+
+    { link: "/aboutus", element: <AboutUsPage />, id: 9 },
+  ];
+  return (
+    <>
+      <Routes>
+        {PUBLIC_ROUTES.map((item) => (
+          <Route path={item.link} element={item.element} key={item.id} />
+        ))}
+      </Routes>
+    </>
+  );
 };
 
 export default MainRoutes;
