667d987c5f2843f86ef0064d3c09d1e3f364cfaf
dxsya <dxgdxd@gmail.com>
2022-11-25 10:57:53 +0600

    .

---
+++ src/components/ContactUs/Map.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+const Map = () => {
+  return (
+    <div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12655.320759671682!2d127.125825!3d37.5355!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357caf1661f8aec3%3A0xae072cacfe478501!2z7J207Iqk7Yq47IS87Yq465-07YOA7JuMIEVhc3QgQ2VudHJhbCBUb3dlcg!5e0!3m2!1sru!2skg!4v1669316211542!5m2!1sru!2skg"
+        width="600"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Map;
