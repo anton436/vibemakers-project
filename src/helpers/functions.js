ca98569b70bbb971a154ba51ccd8ff99dba588e2
unknown <timurshermamatov1@gmail.com>
2022-11-27 22:00:14 +0600

    .

---
+++ src/helpers/functions.js
@@ -0,0 +1,14 @@
+import { CurrencyBitcoin } from "@mui/icons-material";
+
+export function getCountProductsInCart() {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    return cart ? cart.products.length : 0;
+}
+
+export const calcSubPrice = (product) => +product.count * product.item.price;
+
+export const calcTotalPrice = (products) => {
+    return products.reduce((acc, curr) => {
+        return (acc += curr.subPrice);
+    }, 0);
+};
--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import NavabarContexts from "./contexts/NavabarContexts";
 import AuthContextProvider from "./contexts/AuthContextProvider";
 import ProductContextProvider from "./contexts/ProductContextProvider";
+import CartContextProvider from "./contexts/CartContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
-        <ProductContextProvider>
-            <AuthContextProvider>
-                <NavabarContexts>
-                    <App />
-                </NavabarContexts>
-            </AuthContextProvider>
-        </ProductContextProvider>
+        <CartContextProvider>
+            <ProductContextProvider>
+                <AuthContextProvider>
+                    <NavabarContexts>
+                        <App />
+                    </NavabarContexts>
+                </AuthContextProvider>
+            </ProductContextProvider>
+        </CartContextProvider>
     </BrowserRouter>
 );
